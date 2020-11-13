8dc4dde3a8aea30d2e8eb86bcecc844a97cf170e
Vitor Pola <vitor@agilez.com.br>
2020-11-12 22:27:03 -0300

    add validates to name

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,7 +1,6 @@
 class Folder < ApplicationRecord
+  validates_presence_of :name
   has_many_attached :files
   belongs_to :parent, class_name: 'Folder'
   has_many :subfolders, foreign_key: :parent_id, class_name: 'Folder', dependent: :destroy
-
-
 end
